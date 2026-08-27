65ebc5f7e69cf9e38fe8fb3cfbaaf56ff0e06f08
mrkozyrev73 <kozyrant@yandex.ru>
2026-08-27 21:12:20 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='moi-dohody-v58',FILES=['./','./index.html','./app.css?v=14','./supabase-sync.js?v=4','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+const CACHE='moi-dohody-v59',FILES=['./','./index.html','./app.css?v=14','./supabase-sync.js?v=4','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   const request=event.request;
   if(request.mode==='navigate'||new URL(request.url).pathname.endsWith('/index.html')){

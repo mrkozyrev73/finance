06e195d722bc5ac4f72ca71c76e98eddc005a8fd
mrkozyrev73 <kozyrant@yandex.ru>
2026-08-27 21:24:51 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moi-dohody-v60',FILES=['./','./index.html','./app.css?v=14','./supabase-sync.js?v=5','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='moi-dohody-v61',FILES=['./','./index.html','./app.css?v=15','./supabase-sync.js?v=5','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
